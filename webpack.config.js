5d8b1a95f378a7444cfdd9c5430607a3c16f4908
Comfy <kenwadder27@gmail.com>
2023-05-23 10:02:56 -0500

    starting project dir

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: [
+                'style-loader',
+                'css-loader',
+            ],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type:'asset/resource',
+        },
+      ],
+  }
+};
+
+module.exports = config;
